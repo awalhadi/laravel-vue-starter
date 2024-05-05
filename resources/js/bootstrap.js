f5451b54fea544b39f186f6987d9b087fc703461
A Awal Hadi <awalhadi5@gmail.com>
2024-05-05 11:37:12 +0600

    update mitsimenu issue

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 window.axios = axios;
 
+// import metismenu and initialize
+import Metismenu from "metismenu";
+window.metisMenu = Metismenu;
+
 // jquery
 window.$ = require("jquery");
+window.jQuery = window.$;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
